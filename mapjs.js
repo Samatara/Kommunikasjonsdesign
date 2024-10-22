a79922ee81a88ccabee53b0c726951d97d157fe4
Samatar <111729700+Samatara@users.noreply.github.com>
2024-10-22 13:52:19 +0200

    d

--- mapjs.js
+++ mapjs.js
@@ -1,34 +1,11 @@
 function initializeMap() {
     return new Promise((resolve, reject) => {
-        
         const map = L.map('map').setView([59.12032839107434, 11.385684161183917], 13);
 
-        
-        let tilesLoaded = false;
-        let layersLoaded = false;
-
-      
         const tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         }).addTo(map);
 
-        
-        tileLayer.on('load', function () {
-            tilesLoaded = true;
-            checkIfLoaded();
-        });
-
-       
-        const bounds = [
-            [59.07, 11.32],
-            [59.15, 11.46]
-        ];
-
-       
-        map.setMaxBounds(bounds);
-        map.fitBounds(bounds);
-
-       
         const geojsonData = {
             "type": "FeatureCollection",
             "features": [
@@ -37,8 +14,7 @@ function initializeMap() {
                     "properties": {
                         "title": "Niels Stubs Gate 5",
                         "description": "Fire incident at Niels Stubs Gate 5.",
-                        "marker-color": "#ff7c00",
-                        "marker-symbol": "fire"
+                        "img": "./picvideo/brannstasjon.png" 
                     },
                     "geometry": {
                         "type": "Point",
@@ -50,8 +26,7 @@ function initializeMap() {
                     "properties": {
                         "title": "Borgergata 35",
                         "description": "Fire incident at Borgergata 35.",
-                        "marker-color": "#ff7c00",
-                        "marker-symbol": "fire"
+                        "img": "./picvideo/kiwi.png" 
                     },
                     "geometry": {
                         "type": "Point",
@@ -61,40 +36,62 @@ function initializeMap() {
             ]
         };
 
-       
-        const geoJsonLayer = L.geoJSON(geojsonData).addTo(map);
+        const markers = [];
 
-       
-        geoJsonLayer.on('layeradd', function () {
-            layersLoaded = true;
-            checkIfLoaded();
-        });
+        const geoJsonLayer = L.geoJSON(geojsonData, {
+            onEachFeature: function (feature, layer) {
+                if (feature.properties && feature.properties.title) {
+                    
+                    const popupContent = `
+                        <strong>${feature.properties.title}</strong><br>
+                        ${feature.properties.description}<br>
+                        <img src="${feature.properties.img}" alt="${feature.properties.title}" style="width: 100%; height: auto; margin-top: 10px;">
+                    `;
 
-       
-        geoJsonLayer.eachLayer(function (layer) {
-            if (layer.feature.properties && layer.feature.properties.title) {
-                layer.bindPopup("<strong>" + layer.feature.properties.title + "</strong><br>" + layer.feature.properties.description);
+                   
+                    layer.bindPopup(popupContent);
+                    markers.push(layer);  
+                }
             }
-        });
+        }).addTo(map);
 
-       
         setTimeout(function () {
             map.invalidateSize();
         }, 100);
 
-       
-        function checkIfLoaded() {
-            if (tilesLoaded && layersLoaded) {
-                resolve(map); 
-            }
-        }
+        resolve({ map, geoJsonLayer, markers });  
     });
 }
 
-
 window.onload = function () {
-    initializeMap().then((map) => {
-        console.log('Map is fully loaded and ready');
+    initializeMap().then(({ map, geoJsonLayer, markers }) => {
+        const scrollContentMapping = [
+            { id: "content-1", coords: [11.38537581996161, 59.127094702799695], markerIndex: 0 }, 
+            { id: "content-2", coords: [11.3855, 59.1200], markerIndex: 1 }
+        ];
+
+        const contentWrapper = document.querySelector('.content-wrapper');
+        if (!contentWrapper) {
+            console.error('Content wrapper not found!');
+            return;
+        }
+
+        contentWrapper.addEventListener('scroll', function () {
+            const contentSections = document.querySelectorAll('.content p');
+            contentSections.forEach((section, index) => {
+                const rect = section.getBoundingClientRect();
+                if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+                    const { coords, markerIndex } = scrollContentMapping[index];
+                    map.flyTo([coords[1], coords[0]], 15);
+
+                  
+                    markers.forEach(marker => marker.closePopup());
+
+                   
+                    markers[markerIndex].openPopup();
+                }
+            });
+        });
     }).catch((error) => {
         console.error('An error occurred while loading the map', error);
     });
